c94ffda5e496cef0017e8800b33550f0bbf1b76e
BradGillis4 <bradgillis4@gmail.com>
2020-12-07 14:20:14 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, program)
+    puts "Hi, my name is #{name} and I am learning to program in #{program}.\n"
+end
+
+introduction("Brad")
+introduction_with_language("Dan", "Ember.js")
